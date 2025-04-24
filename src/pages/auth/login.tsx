82844eaa04a8c51f648c99cc4b7a457d3545d7ae
Funmilayo <funmilayoibj@gmail.com>
2025-04-24 19:55:21 +0100

    Fix: Added default exports to auth pages

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+// pages/auth/login.tsx
+
+import React from "react";
+
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Login</h1>
+            {/* Your login form or content goes here */}
+        </div>
+    );
+}
---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,12 @@
+// pages/auth/signup.tsx
+
+import React from "react";
+
+export default function SignupPage() {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            {/* Your signup form or content goes here */}
+        </div>
+    );
+}
